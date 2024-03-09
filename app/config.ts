c536a6be5adf4b89ab26f0165a0d8034219bd887
Ajay Asnani <101942594+AjayAsnani@users.noreply.github.com>
2024-03-09 14:56:40 +0530

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,1 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL = 'https://b867-2406-7400-63-3f18-94c7-6ce3-bfa-37a3.ngrok-free.app/';
-process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
-export const SHEET = '1VbuXKUeMQnKjdqqGvqn9Ed0oMPTyLlv_gN1k1GM7KEE';
