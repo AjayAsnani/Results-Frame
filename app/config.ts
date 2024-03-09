a60851e3f454ee140ed085f4106e4f02209d806a
AjayAsnani <ajaysnani555@gmail.com>
2024-03-10 02:02:24 +0530

    new commit

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL = 'https://93f1-2406-7400-63-314a-c848-9055-14f6-1bf.ngrok-free.app';
+export const NEXT_PUBLIC_URL = 'https://fbi-results.vercel.app/';
 // process.env.NODE_ENV == "development"
 // 	? "http://localhost:3000"
 // 	: "https://zizzamia.xyz";
